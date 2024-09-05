44510abb25f61816dc79725f030da3fc87ebdf3a
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-09-05 20:35:59 +0900

    added format on save

--- nvim/.config/nvim/lua/ilovedanchu/init.lua
+++ nvim/.config/nvim/lua/ilovedanchu/init.lua
@@ -9,67 +9,75 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-	require("plenary.reload").reload_module(name)
+  require("plenary.reload").reload_module(name)
 end
 
 vim.filetype.add({
-	extension = {
-		templ = "templ",
-	},
+  extension = {
+    templ = "templ",
+  },
 })
 
 autocmd("TextYankPost", {
-	group = yank_group,
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 40,
-		})
-	end,
+  group = yank_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 40,
+    })
+  end,
 })
 
 autocmd({ "BufWritePre" }, {
-	group = ilovedanchuGroup,
-	pattern = "*",
-	command = [[%s/\s\+$//e]],
+  group = ilovedanchuGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+autocmd({ "BufWritePre" }, {
+  group = ilovedanchuGroup,
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
 })
 
 autocmd("LspAttach", {
-	group = ilovedanchuGroup,
-	callback = function(e)
-		local opts = { buffer = e.buf }
-		vim.keymap.set("n", "gd", function()
-			vim.lsp.buf.definition()
-		end, opts)
-		vim.keymap.set("n", "K", function()
-			vim.lsp.buf.hover()
-		end, opts)
-		vim.keymap.set("n", "<leader>vws", function()
-			vim.lsp.buf.workspace_symbol()
-		end, opts)
-		vim.keymap.set("n", "<leader>vd", function()
-			vim.diagnostic.open_float()
-		end, opts)
-		vim.keymap.set("n", "<leader>vca", function()
-			vim.lsp.buf.code_action()
-		end, opts)
-		vim.keymap.set("n", "<leader>vrr", function()
-			vim.lsp.buf.references()
-		end, opts)
-		vim.keymap.set("n", "<leader>vrn", function()
-			vim.lsp.buf.rename()
-		end, opts)
-		vim.keymap.set("i", "<C-h>", function()
-			vim.lsp.buf.signature_help()
-		end, opts)
-		vim.keymap.set("n", "[d", function()
-			vim.diagnostic.goto_next()
-		end, opts)
-		vim.keymap.set("n", "]d", function()
-			vim.diagnostic.goto_prev()
-		end, opts)
-	end,
+  group = ilovedanchuGroup,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    vim.keymap.set("n", "gd", function()
+      vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "K", function()
+      vim.lsp.buf.hover()
+    end, opts)
+    vim.keymap.set("n", "<leader>vws", function()
+      vim.lsp.buf.workspace_symbol()
+    end, opts)
+    vim.keymap.set("n", "<leader>vd", function()
+      vim.diagnostic.open_float()
+    end, opts)
+    vim.keymap.set("n", "<leader>vca", function()
+      vim.lsp.buf.code_action()
+    end, opts)
+    vim.keymap.set("n", "<leader>vrr", function()
+      vim.lsp.buf.references()
+    end, opts)
+    vim.keymap.set("n", "<leader>vrn", function()
+      vim.lsp.buf.rename()
+    end, opts)
+    vim.keymap.set("i", "<C-h>", function()
+      vim.lsp.buf.signature_help()
+    end, opts)
+    vim.keymap.set("n", "[d", function()
+      vim.diagnostic.goto_next()
+    end, opts)
+    vim.keymap.set("n", "]d", function()
+      vim.diagnostic.goto_prev()
+    end, opts)
+  end,
 })
 
 --vim.g.netrw_browse_split = 0
