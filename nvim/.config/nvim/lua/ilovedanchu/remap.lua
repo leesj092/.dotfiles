9ff7ebbe167e4b8bf001ce98a0c265e97bffc071
leesj092 <61524863+leesj092@users.noreply.github.com>
2024-10-10 03:28:19 +0900

    removed so remap

--- nvim/.config/nvim/lua/ilovedanchu/remap.lua
+++ nvim/.config/nvim/lua/ilovedanchu/remap.lua
@@ -48,7 +48,3 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 --vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
-
-vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
-end)
